d3508adc9aacfd47f06a92a98af6208dff6485d4
MEET <meetvaghasiya3229@gmail.com>
2024-08-07 23:30:00 +0530

    (Day - 1)project create...

---
+++ src/test/java/com/codewithmeet/video_streamingapp/VideoStreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmeet.video_streamingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
